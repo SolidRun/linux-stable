e38f77267a7955b794f04079b455eb6e7deae5a4
Nikhil Badola <nikhil.badola@freescale.com>
2014-12-09 19:00:32 +0530

    usb: gadget: Turn on cache snooping hardware in usb gadget
    Turn on cache snooping hardware in usb gadget for PPC 64-bit Kernel
    
    Signed-off-by: Nikhil Badola <nikhil.badola@freescale.com>
    Signed-off-by: yinbo.zhu <yinbo.zhu@nxp.com>

--- drivers/usb/gadget/udc/fsl_udc_core.c
+++ drivers/usb/gadget/udc/fsl_udc_core.c
@@ -332,6 +332,12 @@ static int dr_controller_setup(struct fsl_udc *udc)
 	}
 #endif
 
+#if (defined(CONFIG_PPC32) || defined(CONFIG_PPC64)) && \
+!defined(CONFIG_NOT_COHERENT_CACHE)
+/* Turn on cache snooping hardware, since some PowerPC platforms
+ * wholly rely on hardware to deal with cache coherent.
+ */
+
 #if !defined(CONFIG_NOT_COHERENT_CACHE)
 	/* Turn on cache snooping hardware, since some PowerPC platforms
 	 * wholly rely on hardware to deal with cache coherent. */
