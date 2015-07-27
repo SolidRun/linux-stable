f573865e90efa787dab154a25535cf94d10331aa
Fugang Duan <b38611@freescale.com>
2015-07-27 15:40:00 +0800

    MLK-10919 net: phy: micrel: add ksz8081 resume function
    Add ksz8081 resume function since the phy has some non-standard
    register init process that has some fixup.
    
    Signed-off-by: Fugang Duan <B38611@freescale.com>
    (cherry picked from commit: 65c6e997b8e020b9e87d1af23c94c15c13e3d2e3)

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -249,7 +249,7 @@ static int phy_needs_fixup(struct phy_device *phydev, struct phy_fixup *fixup)
 }
 
 /* Runs any matching fixups for this phydev */
-static int phy_scan_fixups(struct phy_device *phydev)
+int phy_scan_fixups(struct phy_device *phydev)
 {
 	struct phy_fixup *fixup;
 
@@ -269,6 +269,7 @@ static int phy_scan_fixups(struct phy_device *phydev)
 
 	return 0;
 }
+EXPORT_SYMBOL(phy_scan_fixups);
 
 static int phy_bus_match(struct device *dev, struct device_driver *drv)
 {
