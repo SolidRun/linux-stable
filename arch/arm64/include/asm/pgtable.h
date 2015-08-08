20a98e1b7a4bc50464ad314c8e03f21c3af11230
Haiying Wang <Haiying.wang@freescale.com>
2015-08-08 07:25:02 -0400

    arm64/pgtable: add support to map cacheable and non shareable memory
    Signed-off-by: Haiying Wang <Haiying.wang@freescale.com>

--- arch/arm64/include/asm/pgtable.h
+++ arch/arm64/include/asm/pgtable.h
@@ -405,6 +405,8 @@ static inline int pmd_protnone(pmd_t pmd)
 #define pgprot_cached(prot) \
 	__pgprot_modify(prot, PTE_ATTRINDX_MASK, PTE_ATTRINDX(MT_NORMAL) | \
 			PTE_PXN | PTE_UXN)
+#define pgprot_cached_ns(prot) \
+	__pgprot(pgprot_val(pgprot_cached(prot)) ^ PTE_SHARED)
 #define pgprot_device(prot) \
 	__pgprot_modify(prot, PTE_ATTRINDX_MASK, PTE_ATTRINDX(MT_DEVICE_nGnRE) | PTE_PXN | PTE_UXN)
 #define __HAVE_PHYS_MEM_ACCESS_PROT
