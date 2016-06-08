076a77caa05486cc57f98c39d13808d00def8444
Anson Huang <Anson.Huang@nxp.com>
2016-06-08 18:52:38 +0800

    MLK-12890 rtc: snvs: update time read function
    When CPU/AXI/AHB are running at 24MHz, IPG at
    12MHz, two consecutive reads of RTC timer registers
    never get same value, so we need to skip the low
    15 bits, only make sure the second value are same
    during two consecutive reads.
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>

--- drivers/rtc/rtc-snvs.c
+++ drivers/rtc/rtc-snvs.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2012 Freescale Semiconductor, Inc.
+ * Copyright (C) 2011-2016 Freescale Semiconductor, Inc.
  *
  * The code contained herein is licensed under the GNU General Public
  * License. You may obtain a copy of the GNU General Public License
@@ -73,7 +73,8 @@ static u32 rtc_read_lp_counter(struct snvs_rtc_data *data)
 	do {
 		read2 = read1;
 		read1 = rtc_read_lpsrt(data);
-	} while (read1 != read2 && --timeout);
+	} while ((read1 >> CNTR_TO_SECS_SH) != (read2 >> CNTR_TO_SECS_SH)
+		  && --timeout);
 	if (!timeout)
 		dev_err(&data->rtc->dev, "Timeout trying to get valid LPSRT Counter read\n");
 
