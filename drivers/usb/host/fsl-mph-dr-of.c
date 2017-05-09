48d91289774322afd846a64b18bcd4e714bd7574
Nikhil Badola <nikhil.badola@freescale.com>
2017-05-09 10:15:07 +0800

    usb :fsl: Remove USB Errata checking code
    Remove USB errata checking code from driver. Applicability of erratum
    is retrieved by reading corresponding property in device tree.
    This property is written during device tree fixup.
    
    Signed-off-by: Ramneek Mehresh <ramneek.mehresh@freescale.com>
    Signed-off-by: Nikhil Badola <nikhil.badola@freescale.com>
    Signed-off-by: yinbo.zhu <yinbo.zhu@nxp.com>

--- drivers/usb/host/fsl-mph-dr-of.c
+++ drivers/usb/host/fsl-mph-dr-of.c
@@ -230,6 +230,12 @@ static int fsl_usb2_mph_dr_of_probe(struct platform_device *ofdev)
 	else
 		pdata->has_fsl_erratum_a006918 = 0;
 
+	if (of_get_property(np, "fsl,usb_erratum_14", NULL))
+		pdata->has_fsl_erratum_14 = 1;
+	else
+		pdata->has_fsl_erratum_14 = 0;
+
+
 	/*
 	 * Determine whether phy_clk_valid needs to be checked
 	 * by reading property in device tree
