f22c4b5ffd3d1b12040f9a316dd25b7f54282ede
Robin Gong <yibin.gong@nxp.com>
2017-06-27 10:05:05 +0800

    MLK-15034: dma: imx-sdma: no need report interrupt for channel0
    It is possible for an irq triggered by channel0 to be received later,
    after clks are disabled. If that happens then clearing them by writing
    to SDMA_H_INTR won't work and the system will hang processing infinite
    interrupts. Actually, don't need interrupt triggered on channel0 since
    it's pollling to know channel0 done rather than interrupt in current
    code, just clear BD setting to disable channel0 interrupt to avoid the
    above case.
    
    Reported-by: Leonard Crestez <leonard.crestez@nxp.com>
    Signed-off-by: Robin Gong <yibin.gong@nxp.com>

--- drivers/dma/imx-sdma.c
+++ drivers/dma/imx-sdma.c
@@ -1035,7 +1035,7 @@ static int sdma_save_restore_context(struct sdma_engine *sdma, bool save)
 	else
 		bd0->mode.command = C0_SETDM;
 
-	bd0->mode.status = BD_DONE | BD_INTR | BD_WRAP | BD_EXTD;
+	bd0->mode.status = BD_DONE | BD_WRAP | BD_EXTD;
 	bd0->mode.count = MAX_DMA_CHANNELS * sizeof(*context) / 4;
 	bd0->buffer_addr = sdma->context_phys;
 	bd0->ext_buffer_addr = 2048;
