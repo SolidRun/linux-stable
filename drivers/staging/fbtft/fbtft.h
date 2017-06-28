3bfdba391112811e8c666bc263be2895208770db
Troy Kisky <troy.kisky@boundarydevices.com>
2017-06-28 14:14:59 -0700

    fbtft-bus: use 27 bit words instead of 9 for fbtft_write_vmem16_bus9
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/staging/fbtft/fbtft.h
+++ drivers/staging/fbtft/fbtft.h
@@ -159,6 +159,12 @@ struct fbtft_platform_data {
 	unsigned char tx_high[64] ____cacheline_aligned;
 };
 
+struct txbuf {
+	void *buf;
+	dma_addr_t dma;
+	size_t len;
+};
+
 /**
  * struct fbtft_par - Main FBTFT data structure
  *
@@ -211,11 +217,8 @@ struct fbtft_par {
 	struct fbtft_platform_data *pdata;
 	u16 *ssbuf;
 	u32 pseudo_palette[16];
-	struct {
-		void *buf;
-		dma_addr_t dma;
-		size_t len;
-	} txbuf;
+	struct txbuf txbuf;
+	struct txbuf txbuf2;
 	u8 *buf;
 	u8 startbyte;
 	struct fbtft_ops fbtftops;
