a4314007dc6f309bca85cc40d5bd405aecd2d912
Lukas Wunner <lukas@wunner.de>
2018-01-10 16:32:10 +0100

    Bluetooth: hci_bcm: Sleep instead of spinning
    The driver calls mdelay(15) in the ->suspend, ->resume, ->runtime_suspend
    and ->runtime_resume hook, however spinning for such a long period of
    time is discouraged as per Documentation/timers/timers-howto.txt.
    
    The use of mdelay() seems unnecessary, it is allowed to sleep in the
    system sleep and runtime PM hooks (with the exception of ->suspend_noirq
    and ->resume_noirq) and the driver itself also does not rely on a
    non-sleeping ->runtime_resume as the only place where a synchronous
    resume is performed, in bcm_dequeue(), is called from a work item in
    hci_ldisc.c and hci_serdev.c.
    
    So replace the mdelay(15) with msleep(15).
    
    Note that the delay is inserted after asserting or deasserting the
    device wake pin, but in bcm_gpio_set_power() that pin is asserted or
    deasserted *without* observing a delay.  It is thus unclear if the delay
    is necessary at all.  It is likewise unclear why it is exactly 15 ms,
    the commit introducing it, 118612fb9165 ("Bluetooth: hci_bcm: Add
    suspend/resume PM functions"), does not provide a rationale.
    
    Cc: Frédéric Danis <frederic.danis.oss@gmail.com>
    Suggested-and-reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/hci_bcm.c
+++ drivers/bluetooth/hci_bcm.c
@@ -652,7 +652,7 @@ static int bcm_suspend_device(struct device *dev)
 	}
 
 	bt_dev_dbg(bdev, "suspend, delaying 15 ms");
-	mdelay(15);
+	msleep(15);
 
 	return 0;
 }
@@ -671,7 +671,7 @@ static int bcm_resume_device(struct device *dev)
 	}
 
 	bt_dev_dbg(bdev, "resume, delaying 15 ms");
-	mdelay(15);
+	msleep(15);
 
 	/* When this executes, the device has woken up already */
 	if (bdev->is_suspended && bdev->hu) {
