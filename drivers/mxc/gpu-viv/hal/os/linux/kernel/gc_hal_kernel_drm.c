54ec0a513332928123945b0d6f8be5f8a2f6c3af
Yuchou Gan <yuchou.gan@nxp.com>
2018-01-26 22:34:54 +0800

    MGS-3616 [#ccc] Integrate 6.2.4.p1.pre2 hal driver to linux kernel
    Update the gpu kernel to 6.2.4.p1.pre2
    
    Date: Jan 26, 2017
    Signed-off-by: Yuchou Gan yuchou.gan@nxp.com

--- drivers/mxc/gpu-viv/hal/os/linux/kernel/gc_hal_kernel_drm.c
+++ drivers/mxc/gpu-viv/hal/os/linux/kernel/gc_hal_kernel_drm.c
@@ -2,7 +2,7 @@
 *
 *    The MIT License (MIT)
 *
-*    Copyright (c) 2014 - 2017 Vivante Corporation
+*    Copyright (c) 2014 - 2018 Vivante Corporation
 *
 *    Permission is hereby granted, free of charge, to any person obtaining a
 *    copy of this software and associated documentation files (the "Software"),
@@ -26,7 +26,7 @@
 *
 *    The GPL License (GPL)
 *
-*    Copyright (C) 2014 - 2017 Vivante Corporation
+*    Copyright (C) 2014 - 2018 Vivante Corporation
 *
 *    This program is free software; you can redistribute it and/or
 *    modify it under the terms of the GNU General Public License
@@ -165,7 +165,7 @@ static int viv_ioctl_gem_create(struct drm_device *drm, void *data,
     gckKERNEL kernel;
     gctUINT32 processID;
     gckVIDMEM_NODE nodeObject;
-    gctUINT32 flags = 0;
+    gctUINT32 flags = gcvALLOC_FLAG_DMABUF_EXPORTABLE;
     gceSTATUS status = gcvSTATUS_OK;
 
     gal_dev = (gckGALDEVICE)drm->dev_private;
