bdfbcc776d93b33bfba6cb801820b3fa9ec8637b
Jon Nettleton <jon@solid-run.com>
2018-04-26 15:32:09 +0200

    gpu: galcore: Optimize gckEVENT_Notify
    Don't bother trying to get the eventQueueMutex unless we have
    pending events and are going to do something with them.
    
    Signed-off-by: Jon Nettleton <jon@solid-run.com>

--- drivers/mxc/gpu-viv/hal/kernel/gc_hal_kernel_event.c
+++ drivers/mxc/gpu-viv/hal/kernel/gc_hal_kernel_event.c
@@ -2332,12 +2332,6 @@ gckEVENT_Notify(
     {
         gcsEVENT_PTR record;
 
-        /* Grab the mutex queue. */
-        gcmkONERROR(gckOS_AcquireMutex(Event->os,
-                                       Event->eventQueueMutex,
-                                       gcvINFINITE));
-        acquired = gcvTRUE;
-
         gckOS_AtomGet(Event->os, Event->pending, (gctINT32_PTR)&pending);
 
         if (pending == 0)
@@ -2384,6 +2378,12 @@ gckEVENT_Notify(
             );
 
         queue = gcvNULL;
+
+        /* Grab the mutex queue. */
+        gcmkONERROR(gckOS_AcquireMutex(Event->os,
+                                       Event->eventQueueMutex,
+                                       gcvINFINITE));
+        acquired = gcvTRUE;
 
         gcmDEBUG_ONLY(
             if (IDs == 0)
