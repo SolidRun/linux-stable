f6294dbd1e3d514ca737b0a241006f0aa70a5abd
Roy Pledge <roy.pledge@nxp.com>
2018-10-26 10:48:49 -0400

    bus: fsl-mc: Add support for mapping shareable portals
    Starting with v5 of NXP QBMan devices the hardware supports using
    regular cacheable/shareable memory as the backing store for the
    portals.
    
    This patch adds support for the new portal mode by switching to
    use the DPRC get object region v2 command which returns both
    a base address and offset for the portal memory. The new portal
    region is identified as shareable through the addition of a new
    flag.
    
    Signed-off-by: Roy Pledge <roy.pledge@nxp.com>

--- drivers/bus/fsl-mc/dprc.c
+++ drivers/bus/fsl-mc/dprc.c
@@ -502,10 +502,11 @@ int dprc_get_obj_region(struct fsl_mc_io *mc_io,
 
 	/* retrieve response parameters */
 	rsp_params = (struct dprc_rsp_get_obj_region *)cmd.params;
-	region_desc->base_offset = le32_to_cpu(rsp_params->base_addr);
+	region_desc->base_offset = le32_to_cpu(rsp_params->base_offset);
 	region_desc->size = le32_to_cpu(rsp_params->size);
 	region_desc->type = rsp_params->type;
 	region_desc->flags = le32_to_cpu(rsp_params->flags);
+	region_desc->base_address = le64_to_cpu(rsp_params->base_addr);
 
 	return 0;
 }
