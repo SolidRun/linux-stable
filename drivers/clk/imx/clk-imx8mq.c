analogdevicesinc/linux

MLK-20136-02 driver: clk: imx: keep DRAM PLL always on for i.MX8MQ
Keep the DRAM PLL always on by default on i.MX8MQ.

Signed-off-by: Bai Ping <ping.bai@nxp.com>
Reviewed-by: Anson Huang <Anson.Huang@nxp.com>

drivers/clk/imx/clk-imx8mq.c
@@ -281,7 +281,7 @@ static int const clks_init_on[] __initconst = {
 	IMX8MQ_CLK_USB_BUS,
 	IMX8MQ_CLK_MAIN_AXI, IMX8MQ_CLK_A53_CG,
 	IMX8MQ_CLK_TMU_ROOT,
-	IMX8MQ_CLK_DRAM_APB,
+	IMX8MQ_CLK_DRAM_APB, IMX8MQ_DRAM_PLL_OUT,
 };
 
 static struct clk ** const uart_clks[] __initconst = {

SolidRun/linux-stable

drivers/clk/imx/clk-imx8mq.c
@@ -284,7 +284,7 @@ static int const clks_init_on[] __initconst = {
 	IMX8MQ_CLK_USB_BUS_CG,
 	IMX8MQ_CLK_MAIN_AXI_CG, IMX8MQ_CLK_A53_CG,
 	IMX8MQ_CLK_TMU_ROOT,
-	IMX8MQ_CLK_DRAM_APB_DIV,
+	IMX8MQ_CLK_DRAM_APB_DIV, IMX8MQ_DRAM_PLL_OUT,
 };
 
 static struct clk_onecell_data clk_data;