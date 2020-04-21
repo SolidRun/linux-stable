6c2c638b56aa1676d031dd76369951170d3b4807
Russell King <rmk+kernel@armlinux.org.uk>
2020-04-21 19:20:14 +0100

    net: phy: add supported_interfaces to bcm84881
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- drivers/net/phy/bcm84881.c
+++ drivers/net/phy/bcm84881.c
@@ -70,6 +70,10 @@ static int bcm84881_probe(struct phy_device *phydev)
 	    (phydev->c45_ids.devices_in_package & mmd_mask) != mmd_mask)
 		return -ENODEV;
 
+	__set_bit(PHY_INTERFACE_MODE_SGMII, phydev->supported_interfaces);
+	__set_bit(PHY_INTERFACE_MODE_2500BASEX, phydev->supported_interfaces);
+	__set_bit(PHY_INTERFACE_MODE_10GBASER, phydev->supported_interfaces);
+
 	return 0;
 }
 
