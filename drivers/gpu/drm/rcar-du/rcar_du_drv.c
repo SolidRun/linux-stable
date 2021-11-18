b02226b2f83c0f6c30b8fada9e3f38ef3fa468fd
hienhuynh <hien.huynh.px@renesas.com>
2021-11-18 14:41:25 +0700

    drm: rcar-du: Add DU device information for RZ/G2L
    This commit adds DU device information for RZ/G2L SoC.
    In RZ/G2L, DU device has 1 channel and supports 2 outputs RGB
    and MIPI DSI. We add DSI output definition because DSI device is newly
    supported.
    Also add a feature to distinguish RZ/G2L family from others.
    
    Signed-off-by: hienhuynh <hien.huynh.px@renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_drv.c
+++ drivers/gpu/drm/rcar-du/rcar_du_drv.c
@@ -216,6 +216,25 @@ static const struct rcar_du_device_info rcar_du_r8a774e1_info = {
 	.dpll_mask =  BIT(1),
 };
 
+static const struct rcar_du_device_info rcar_du_rzg2l_info = {
+	.gen = 3,
+	.features = RCAR_DU_FEATURE_CRTC_IRQ_CLOCK
+		  | RCAR_DU_FEATURE_RZG2L
+		  | RCAR_DU_FEATURE_VSP1_SOURCE,
+	.channels_mask = BIT(0),
+	.routes = {
+		[RCAR_DU_OUTPUT_DPAD0] = {
+			.possible_crtcs = BIT(0),
+			.port = 0,
+		},
+		[RCAR_DU_OUTPUT_MIPI_DSI0] = {
+			.possible_crtcs = BIT(0),
+			.port = 1,
+		},
+	},
+	.num_mipi_dsi = 1,
+};
+
 static const struct rcar_du_device_info rcar_du_r8a7779_info = {
 	.gen = 1,
 	.features = RCAR_DU_FEATURE_INTERLACED
@@ -484,6 +503,7 @@ static const struct of_device_id rcar_du_of_table[] = {
 	{ .compatible = "renesas,du-r8a774b1", .data = &rcar_du_r8a774b1_info },
 	{ .compatible = "renesas,du-r8a774c0", .data = &rcar_du_r8a774c0_info },
 	{ .compatible = "renesas,du-r8a774e1", .data = &rcar_du_r8a774e1_info },
+	{ .compatible = "renesas,du-rzg2l", .data = &rcar_du_rzg2l_info },
 	{ .compatible = "renesas,du-r8a7779", .data = &rcar_du_r8a7779_info },
 	{ .compatible = "renesas,du-r8a7790", .data = &rcar_du_r8a7790_info },
 	{ .compatible = "renesas,du-r8a7791", .data = &rcar_du_r8a7791_info },
