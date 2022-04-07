ab4881c8b7312137996a4fd8d50453b68e6bcbba
Hao Bui <hao.bui.yg@renesas.com>
2022-04-07 11:51:30 +0700

    ARM: shmobile: Export the address of reserved CMA area
    This patch makes rcar_gen2_dma_contiguous a global static variable and
    export it for usage in out-of-tree kernel modules.
    
    Signed-off-by: Hao Bui <hao.bui.yg@renesas.com>

--- arch/arm/mach-shmobile/setup-rcar-gen2.c
+++ arch/arm/mach-shmobile/setup-rcar-gen2.c
@@ -174,6 +174,9 @@ static int __init rcar_gen2_scan_mem(unsigned long node, const char *uname,
 	return 0;
 }
 
+struct cma *rcar_gen2_dma_contiguous;
+EXPORT_SYMBOL(rcar_gen2_dma_contiguous);
+
 static void __init rcar_gen2_reserve(void)
 {
 	struct memory_reserve_config mrc;
@@ -185,8 +188,6 @@ static void __init rcar_gen2_reserve(void)
 	of_scan_flat_dt(rcar_gen2_scan_mem, &mrc);
 #ifdef CONFIG_DMA_CMA
 	if (mrc.size && memblock_is_region_memory(mrc.base, mrc.size)) {
-		static struct cma *rcar_gen2_dma_contiguous;
-
 		dma_contiguous_reserve_area(mrc.size, mrc.base, 0,
 					    &rcar_gen2_dma_contiguous, true);
 	}
