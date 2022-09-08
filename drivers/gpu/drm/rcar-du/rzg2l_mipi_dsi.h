6906b575b2a4979a826d01b0ea3f6ae509042017
hienhuynh <hien.huynh.px@renesas.com>
2022-09-08 07:40:30 +0700

    drm: rzg2l_mipi_dsi: add an API to get number of MIPI DSI data lanes
    This commit adds an API to get number of MIPI DSI data lanes.
    It is used to set PLL clock rate for RZ/G2L Series in next commits.
    
    Signed-off-by: hienhuynh <hien.huynh.px@renesas.com>

--- drivers/gpu/drm/rcar-du/rzg2l_mipi_dsi.h
+++ drivers/gpu/drm/rcar-du/rzg2l_mipi_dsi.h
@@ -13,6 +13,7 @@ struct drm_bridge;
 #if IS_ENABLED(CONFIG_DRM_RZG2L_MIPI_DSI)
 int rzg2l_mipi_dsi_clk_enable(struct drm_bridge *bridge);
 void rzg2l_mipi_dsi_clk_disable(struct drm_bridge *bridge);
+int rzg2l_mipi_dsi_get_data_lanes(struct drm_bridge *bridge);
 #else
 static inline int rzg2l_mipi_dsi_clk_enable(struct drm_bridge *bridge)
 {
@@ -20,6 +21,10 @@ static inline int rzg2l_mipi_dsi_clk_enable(struct drm_bridge *bridge)
 }
 static inline void rzg2l_mipi_dsi_clk_disable(struct drm_bridge *bridge)
 {
+}
+static inline int rzg2l_mipi_dsi_get_data_lanes(struct drm_bridge *bridge)
+{
+	return 0;
 }
 #endif /* CONFIG_DRM_RZG2L_MIPI_DSI */
 #endif /* __RZG2L_MIPI_DSI_H__ */
