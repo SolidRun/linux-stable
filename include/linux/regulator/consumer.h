c5fdcfe6c07ae7e705349881ce32d7a528239471
Zev Weiss <zev@bewilderbeest.net>
2022-10-31 16:37:02 -0700

    regulator: devres: Add devm_regulator_bulk_get_exclusive()
    We had an exclusive variant of the devm_regulator_get() API, but no
    corresponding variant for the bulk API; let's add one now.  We add a
    generalized version of the existing regulator_bulk_get() function that
    additionally takes a get_type parameter and redefine
    regulator_bulk_get() in terms of it, then do similarly with
    devm_regulator_bulk_get(), and finally add the new
    devm_regulator_bulk_get_exclusive().
    
    Signed-off-by: Zev Weiss <zev@bewilderbeest.net>
    Link: https://lore.kernel.org/r/20221031233704.22575-2-zev@bewilderbeest.net
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/linux/regulator/consumer.h
+++ include/linux/regulator/consumer.h
@@ -249,6 +249,8 @@ int __must_check of_regulator_bulk_get_all(struct device *dev, struct device_nod
 int __must_check devm_regulator_bulk_get(struct device *dev, int num_consumers,
 					 struct regulator_bulk_data *consumers);
 void devm_regulator_bulk_put(struct regulator_bulk_data *consumers);
+int __must_check devm_regulator_bulk_get_exclusive(struct device *dev, int num_consumers,
+						   struct regulator_bulk_data *consumers);
 int __must_check devm_regulator_bulk_get_const(
 	struct device *dev, int num_consumers,
 	const struct regulator_bulk_data *in_consumers,
