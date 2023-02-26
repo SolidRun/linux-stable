ed7d51afa76ef6d7f1e7f36924ac6d1c672732b6
Biju Das <biju.das.jz@bp.renesas.com>
2023-02-26 15:49:57 +0000

    usb: renesas_usbhs: Add reset_control_{assert,deassert} in PM callbacks
    Add assertion/deassertion of reset control in PM_SLEEP suspend/resume
    callbacks of USBHS driver as a requirement on RZ/G2L/G3S platforms.
    
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>
    Signed-off-by: tuleanh <tu.le-anh@banvien.com.vn>

--- drivers/usb/renesas_usbhs/common.c
+++ drivers/usb/renesas_usbhs/common.c
@@ -812,6 +812,8 @@ static __maybe_unused int usbhsc_suspend(struct device *dev)
 	if (mod || !usbhs_get_dparam(priv, runtime_pwctrl))
 		usbhsc_power_ctrl(priv, 0);
 
+	reset_control_assert(priv->rsts);
+
 	return 0;
 }
 
@@ -819,6 +821,8 @@ static __maybe_unused int usbhsc_resume(struct device *dev)
 {
 	struct usbhs_priv *priv = dev_get_drvdata(dev);
 	struct platform_device *pdev = usbhs_priv_to_pdev(priv);
+
+	reset_control_deassert(priv->rsts);
 
 	if (!usbhs_get_dparam(priv, runtime_pwctrl)) {
 		usbhsc_power_ctrl(priv, 1);
