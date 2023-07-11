ea0c959beb568e4754417b27ccb28c88c0e3b9e8
Tai Huynh <tai.huynh.cp@renesas.com>
2023-07-11 17:44:25 +0700

    drivers: mmc: renesas_sdhi: Add RZ/V2M compatible string
    The SDHI/eMMC IPs found with the RZ/V2M (a.k.a. r9a09g011), are
    
    very similar to the ones found in R-Car Gen3, but they are not
    
    exactly the same, and as a result need an SoC specific compatible
    
    string for fine tuning driver support.
    
    Signed-off-by: Phil Edworthy <phil.edworthy@renesas.com>

--- drivers/mmc/host/renesas_sdhi_internal_dmac.c
+++ drivers/mmc/host/renesas_sdhi_internal_dmac.c
@@ -127,6 +127,7 @@ static const struct of_device_id renesas_sdhi_internal_dmac_of_match[] = {
 	{ .compatible = "renesas,sdhi-mmc-r8a77470", .data = &of_rcar_gen3_compatible, },
 	{ .compatible = "renesas,sdhi-r8a7795", .data = &of_rcar_gen3_compatible, },
 	{ .compatible = "renesas,sdhi-r8a7796", .data = &of_rcar_gen3_compatible, },
+	{ .compatible = "renesas,sdhi-r9a09g011", .data = &of_rcar_gen3_compatible, },
 	{ .compatible = "renesas,rcar-gen3-sdhi", .data = &of_rcar_gen3_compatible, },
 	{},
 };
