54ae1f0ab05fee13f45aeeda41c6dd16be954ece
Tai Huynh <tai.huynh.cp@renesas.com>
2023-07-14 16:48:43 +0700

    drivers: net: ethernet: ravb: Add AVB support for RZ/V2MA
    RZ/V2MA Ethernet is very similar to R-Car Gen3 Ethernet-AVB, though
    
    some small parts are the same as R-Car Gen2.
    
    Add compatible to support Ethernet driver for RZ/V2MA SoC
    
    Signed-off-by: Tai Huynh <tai.huynh.cp@renesas.com>

--- drivers/net/ethernet/renesas/ravb_main.c
+++ drivers/net/ethernet/renesas/ravb_main.c
@@ -2508,6 +2508,7 @@ static const struct of_device_id ravb_match_table[] = {
 	{ .compatible = "renesas,etheravb-r8a7795", .data = &ravb_gen3_hw_info },
 	{ .compatible = "renesas,etheravb-rcar-gen3", .data = &ravb_gen3_hw_info },
 	{ .compatible = "renesas,etheravb-rzv2m", .data = &ravb_rzv2m_hw_info },
+	{ .compatible = "renesas,etheravb-rzv2ma", .data = &ravb_rzv2m_hw_info },
 	{ .compatible = "renesas,rzg2l-gbeth", .data = &gbeth_hw_info },
 	{ }
 };
