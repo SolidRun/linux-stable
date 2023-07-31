9bc309beb611a497f1c95929bc4c06d8d5706cab
Tai Huynh <tai.huynh.cp@renesas.com>
2023-07-31 18:36:42 +0700

    drivers: mmc: renesas_sdhi: Add RZ/V2MA compatible string
    The SDHI/eMMC IPs found with the RZ/V2MA (a.k.a. r9a09g055), are
    
    very similar to the ones found in R-Car Gen3, but they are not
    
    exactly the same, and as a result need an SoC specific compatible
    
    exactly the same, and as a result need an SoC specific compatible
    
    Signed-off-by: Tai Huynh <tai.huynh.cp@renesas.com>

--- drivers/mmc/host/renesas_sdhi_internal_dmac.c
+++ drivers/mmc/host/renesas_sdhi_internal_dmac.c
@@ -128,6 +128,7 @@ static const struct of_device_id renesas_sdhi_internal_dmac_of_match[] = {
 	{ .compatible = "renesas,sdhi-r8a7795", .data = &of_rcar_gen3_compatible, },
 	{ .compatible = "renesas,sdhi-r8a7796", .data = &of_rcar_gen3_compatible, },
 	{ .compatible = "renesas,sdhi-r9a09g011", .data = &of_rcar_gen3_compatible, },
+	{ .compatible = "renesas,sdhi-r9a09g055", .data = &of_rcar_gen3_compatible, },
 	{ .compatible = "renesas,rcar-gen3-sdhi", .data = &of_rcar_gen3_compatible, },
 	{},
 };
@@ -402,6 +403,8 @@ static const struct soc_device_attribute soc_dma_quirks[] = {
 	{ .soc_id = "r8a7796", .revision = "ES1.0",
 	  .data = (void *)BIT(SDHI_INTERNAL_DMAC_ONE_RX_ONLY) },
 	{ .soc_id = "r9a09g011",
+	  .data = (void *)BIT(SDHI_INTERNAL_DMAC_ADDR_MODE_FIXED_ONLY) },
+	{ .soc_id = "r9a09g055",
 	  .data = (void *)BIT(SDHI_INTERNAL_DMAC_ADDR_MODE_FIXED_ONLY) },
 	{ /* sentinel */ }
 };
