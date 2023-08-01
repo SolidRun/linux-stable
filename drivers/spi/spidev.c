2b89360bc130efb24eb8802ae331e1f9c69d76f2
Tai Huynh <tai.huynh.cp@renesas.com>
2023-08-01 14:33:38 +0700

    drivers: spi: spidev: Add RZ/V2M compatible
    Add compatible to support CSI slave mode on RZ/V2M
    
    Signed-off-by: Tai Huynh <tai.huynh.cp@renesas.com>

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -691,6 +691,7 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "lwn,bk4" },
 	{ .compatible = "dh,dhcom-board" },
 	{ .compatible = "menlo,m53cpld" },
+	{ .compatible = "rzv2m,slavedev" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
