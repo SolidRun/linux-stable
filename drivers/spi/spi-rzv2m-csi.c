fa7bff7be5a4e7aca689a043a13d3d86384155d4
Tai Huynh <tai.huynh.cp@renesas.com>
2023-10-10 14:36:18 +0700

    drivers: spi: Support slave mode for RZ/V2{M,MA}
    Add source definition to support CSI slave mode for RZ/V2{M,MA}
    
    Signed-off-by: Fabrizio Castro <castro.fabrizio.jz@renesas.com>

--- drivers/spi/spi-rzv2m-csi.c
+++ drivers/spi/spi-rzv2m-csi.c
@@ -9,6 +9,8 @@
 #include <linux/count_zeros.h>
 #include <linux/interrupt.h>
 #include <linux/iopoll.h>
+#include <linux/of.h>
+#include <linux/property.h>
 #include <linux/platform_device.h>
 #include <linux/reset.h>
 #include <linux/spi/spi.h>
@@ -35,6 +37,9 @@
 #define CSI_MODE_SETUP		0x00000040
 
 /* CSI_CLKSEL */
+#define CSI_CLKSEL_SS_ENA	BIT(19)
+#define CSI_CLKSEL_SS_POL	BIT(18)
+#define CSI_CLKSEL_SS		(CSI_CLKSEL_SS_ENA|CSI_CLKSEL_SS_POL)
 #define CSI_CLKSEL_CKP		BIT(17)
 #define CSI_CLKSEL_DAP		BIT(16)
 #define CSI_CLKSEL_SLAVE	BIT(15)
@@ -73,6 +78,15 @@
 
 #define CSI_MAX_SPI_SCKO	8000000
 
+#define CSI_CLKSEL_SS_DISABLED			0
+#define CSI_CLKSEL_SS_ENABLED_ACTIVE_LOW	BIT(1)
+#define CSI_CLKSEL_SS_ENABLED_ACTIVE_HIGH	GENMASK(1, 0)
+
+enum {
+	RZV2M_CSI_SPI_MASTER,
+	RZV2M_CSI_SPI_SLAVE,
+};
+
 struct rzv2m_csi_priv {
 	void __iomem *base;
 	struct clk *csiclk;
@@ -90,6 +104,9 @@ struct rzv2m_csi_priv {
 	wait_queue_head_t wait;
 	u8 errors;
 	u32 status;
+	int mode;
+	int slave_select;
+	bool slave_aborted;
 };
 
 static const unsigned char x_trg[] = {
@@ -199,6 +216,14 @@ static int rzv2m_csi_read_rxfifo(struct rzv2m_csi_priv *csi)
 	return 0;
 }
 
+static inline void rzv2m_csi_empty_rxfifo(struct rzv2m_csi_priv *csi)
+{
+	unsigned int i;
+
+	for (i = 0; i < csi->words_to_transfer; i++)
+		readl(csi->base + CSI_IFIFO);
+}
+
 static inline void rzv2m_csi_calc_current_transfer(struct rzv2m_csi_priv *csi)
 {
 	int bytes_transferred = max_t(int, csi->bytes_received, csi->bytes_sent);
@@ -285,19 +310,24 @@ static int rzv2m_csi_wait_for_interrupt(struct rzv2m_csi_priv *csi,
 
 	rzv2m_csi_enable_irqs(csi, enable_bits);
 
-	ret = wait_event_timeout(csi->wait,
-				 ((csi->status & wait_mask) == wait_mask) ||
-				 csi->errors, HZ);
+	if (csi->mode == RZV2M_CSI_SPI_SLAVE) {
+		ret = wait_event_interruptible(csi->wait,
+				((csi->status & wait_mask) == wait_mask) ||
+				csi->errors || csi->slave_aborted);
+		if (ret || csi->slave_aborted)
+			ret = -EINTR;
+	} else {
+		ret = wait_event_timeout(csi->wait,
+				((csi->status & wait_mask) == wait_mask) ||
+				csi->errors, HZ) == 0 ? -ETIMEDOUT : 0;
+	}
 
 	rzv2m_csi_disable_irqs(csi, enable_bits);
 
 	if (csi->errors)
 		return -EIO;
 
-	if (!ret)
-		return -ETIMEDOUT;
-
-	return 0;
+	return ret;
 }
 
 static int rzv2m_csi_wait_for_tx_empty(struct rzv2m_csi_priv *csi)
@@ -412,8 +442,14 @@ static int rzv2m_csi_setup(struct spi_device *spi)
 	rzv2m_csi_reg_write_bit(csi, CSI_MODE, CSI_MODE_DIR,
 				!!(spi->mode & SPI_LSB_FIRST));
 
-	/* Set the operation mode as master */
-	rzv2m_csi_reg_write_bit(csi, CSI_CLKSEL, CSI_CLKSEL_SLAVE, 0);
+	/* Set the role, 1 for slave and 0 for master */
+	rzv2m_csi_reg_write_bit(csi, CSI_CLKSEL, CSI_CLKSEL_SLAVE,
+				csi->mode == RZV2M_CSI_SPI_SLAVE);
+
+	if (csi->mode == RZV2M_CSI_SPI_SLAVE)
+		/* Configure the slave select pin */
+		rzv2m_csi_reg_write_bit(csi, CSI_CLKSEL, CSI_CLKSEL_SS,
+					csi->slave_select);
 
 	/* Give the IP a SW reset */
 	ret = rzv2m_csi_sw_reset(csi, 1);
@@ -441,9 +477,13 @@ static int rzv2m_csi_pio_transfer(struct rzv2m_csi_priv *csi)
 	/* Make sure the TX FIFO is empty */
 	writel(0, csi->base + CSI_OFIFOL);
 
+	/* Make sure the RX FIFO is empty */
+	writel(0, csi->base + CSI_IFIFOL);
+
 	csi->bytes_sent = 0;
 	csi->bytes_received = 0;
 	csi->errors = 0;
+	csi->slave_aborted = false;
 
 	rzv2m_csi_disable_all_irqs(csi);
 	rzv2m_csi_clear_all_irqs(csi);
@@ -460,29 +500,19 @@ static int rzv2m_csi_pio_transfer(struct rzv2m_csi_priv *csi)
 		rzv2m_csi_calc_current_transfer(csi);
 		rzv2m_csi_set_rx_fifo_trigger_level(csi);
 
-		rzv2m_csi_enable_irqs(csi, CSI_INT_OVERF | CSI_INT_UNDER);
-
-		/* Make sure the RX FIFO is empty */
-		writel(0, csi->base + CSI_IFIFOL);
+		rzv2m_csi_enable_irqs(csi, CSI_CNT_OVERF_E | CSI_CNT_UNDER_E);
 
 		writel(readl(csi->base + CSI_INT), csi->base + CSI_INT);
 		csi->status = 0;
-
-		rzv2m_csi_start_stop_operation(csi, 1, false);
 
 		/* TX */
 		if (csi->txbuf) {
 			ret = rzv2m_csi_fill_txfifo(csi);
 			if (ret)
 				break;
-
-			ret = rzv2m_csi_wait_for_tx_empty(csi);
-			if (ret)
-				break;
-
-			if (csi->bytes_sent == csi->buffer_len)
-				tx_completed = true;
 		}
+
+		rzv2m_csi_start_stop_operation(csi, 1, false);
 
 		/*
 		 * Make sure the RX FIFO contains the desired number of words.
@@ -493,31 +523,31 @@ static int rzv2m_csi_pio_transfer(struct rzv2m_csi_priv *csi)
 		if (ret)
 			break;
 
+		if (csi->mode == RZV2M_CSI_SPI_MASTER) 
+			rzv2m_csi_start_stop_operation(csi, 0, false); 
+
 		/* RX */
 		if (csi->rxbuf) {
-			rzv2m_csi_start_stop_operation(csi, 0, false);
-
 			ret = rzv2m_csi_read_rxfifo(csi);
 			if (ret)
 				break;
 
 			if (csi->bytes_received == csi->buffer_len)
 				rx_completed = true;
+		} else {
+			rzv2m_csi_empty_rxfifo(csi);
 		}
 
-		ret = rzv2m_csi_start_stop_operation(csi, 0, true);
-		if (ret)
-			goto pio_quit;
+		if (csi->bytes_sent == csi->buffer_len) 
+			tx_completed = true; 
 
 		if (csi->errors) {
 			ret = -EIO;
-			goto pio_quit;
+			break;
 		}
 	}
 
 	rzv2m_csi_start_stop_operation(csi, 0, true);
-
-pio_quit:
 	rzv2m_csi_disable_all_irqs(csi);
 	rzv2m_csi_enable_rx_trigger(csi, false);
 	rzv2m_csi_clear_all_irqs(csi);
@@ -539,7 +569,8 @@ static int rzv2m_csi_transfer_one(struct spi_controller *controller,
 
 	rzv2m_csi_setup_operating_mode(csi, transfer);
 
-	rzv2m_csi_setup_clock(csi, transfer->speed_hz);
+	if (csi->mode == RZV2M_CSI_SPI_MASTER)
+		rzv2m_csi_setup_clock(csi, transfer->speed_hz);
 
 	ret = rzv2m_csi_pio_transfer(csi);
 	if (ret) {
@@ -556,24 +587,59 @@ static int rzv2m_csi_transfer_one(struct spi_controller *controller,
 	return ret;
 }
 
+static int rzv2m_csi_slave_abort(struct spi_controller *ctlr)
+{
+	struct rzv2m_csi_priv *csi = spi_controller_get_devdata(ctlr);
+
+	csi->slave_aborted = true;
+	wake_up(&csi->wait);
+
+	return 0;
+}
+
 static int rzv2m_csi_probe(struct platform_device *pdev)
 {
 	struct spi_controller *controller;
 	struct device *dev = &pdev->dev;
 	struct rzv2m_csi_priv *csi;
 	struct reset_control *rstc;
+	struct device_node *np = pdev->dev.of_node;
 	int irq;
+	int mode;
 	int ret;
 
-	controller = devm_spi_alloc_master(dev, sizeof(*csi));
+	mode = of_property_read_bool(np, "spi-slave") ? RZV2M_CSI_SPI_SLAVE :
+							RZV2M_CSI_SPI_MASTER;
+
+	if (mode == RZV2M_CSI_SPI_MASTER)
+		controller = devm_spi_alloc_master(dev, sizeof(*csi));
+	else
+		controller = devm_spi_alloc_slave(dev, sizeof(*csi));
+
 	if (!controller)
 		return -ENOMEM;
 
 	csi = spi_controller_get_devdata(controller);
 	platform_set_drvdata(pdev, csi);
 
+	if (mode == RZV2M_CSI_SPI_SLAVE) {
+		if (of_property_read_bool(np, "renesas,csi-ss")) {
+			if (of_property_read_bool(np, "renesas,csi-ss-high")) {
+				csi->slave_select =
+					CSI_CLKSEL_SS_ENABLED_ACTIVE_HIGH;
+			} else {
+				csi->slave_select =
+					CSI_CLKSEL_SS_ENABLED_ACTIVE_LOW;
+			}
+		} else {
+			csi->slave_select = CSI_CLKSEL_SS_DISABLED;
+		}
+	}
+
 	csi->dev = dev;
 	csi->controller = controller;
+	csi->mode = mode;
+	csi->slave_aborted = false;
 
 	csi->base = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(csi->base))
@@ -605,6 +671,7 @@ static int rzv2m_csi_probe(struct platform_device *pdev)
 	controller->setup = rzv2m_csi_setup;
 	controller->transfer_one = rzv2m_csi_transfer_one;
 	controller->use_gpio_descriptors = true;
+	controller->slave_abort = rzv2m_csi_slave_abort;
 
 	ret = devm_request_irq(dev, irq, rzv2m_csi_irq_handler, 0,
 			       dev_name(dev), csi);
