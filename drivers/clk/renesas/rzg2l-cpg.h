d9f902574df4727e6b3beb7c65f8f9cf4a0550ee
Huy Nguyen <huy.nguyen.wh@renesas.com>
2023-11-28 11:54:11 +0700

    clk: renesas: rzg2l-cpg: Add support to get value CPG_WDTOVF_RST register
    This commit supports to get value CPG_WDTOVF_RST register.
    
    Signed-off-by: Huy Nguyen <huy.nguyen.wh@renesas.com>

--- drivers/clk/renesas/rzg2l-cpg.h
+++ drivers/clk/renesas/rzg2l-cpg.h
@@ -10,6 +10,7 @@
 #define __RENESAS_RZG2L_CPG_H__
 
 #include <linux/notifier.h>
+#include <linux/clk-provider.h>
 
 #define CPG_SIPLL5_STBY		(0x140)
 #define CPG_SIPLL5_CLK1		(0x144)
@@ -27,6 +28,7 @@
 #define CPG_PL6_ETH_SSEL	(0x418)
 #define CPG_PL5_SDIV		(0x420)
 #define CPG_RST_MON		(0x680)
+#define CPG_WDTOVF_RST		(0xB10)
 #define CPG_OTHERFUNC1_REG	(0xBE8)
 
 #define CPG_SIPLL5_STBY_RESETB		BIT(0)
@@ -69,6 +71,8 @@
 #define SEL_GPU2	SEL_PLL_PACK(CPG_PL6_SSEL, 12, 1)
 
 #define EXTAL_FREQ_IN_MEGA_HZ	(24)
+
+#define WDTOVF_WEN(x)	((x) << 16)
 
 /**
  * Definitions of CPG Core Clocks
@@ -280,6 +284,8 @@ struct rzg2l_cpg_info {
 
 	bool has_clk_mon_regs;
 };
+
+unsigned int rzg2l_cpg_wdt_ovf_sysrst(struct clk_hw *hw, int channel);
 
 extern const struct rzg2l_cpg_info r9a07g043_cpg_info;
 extern const struct rzg2l_cpg_info r9a07g044_cpg_info;
