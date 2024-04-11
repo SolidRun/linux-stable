d459a96c5d7afb1f2fecdf3167d0809552b12a15
Thang Dinh <thang.dinh.fv@renesas.com>
2024-04-11 10:05:33 +0700

    dt-bindings: irqc-rzg2l: extend the RZG2L_IRQ macros
    RZ/G3E and RZ/V2H can support upto 16 IRQC interrupts but only 8 IRQCs
    are defined now.
    So, extend the macro index to 16 IRQCs.
    
    Signed-off-by: Thang Dinh <thang.dinh.fv@renesas.com>

--- include/dt-bindings/interrupt-controller/irqc-rzg2l.h
+++ include/dt-bindings/interrupt-controller/irqc-rzg2l.h
@@ -2,7 +2,7 @@
 /*
  * This header provides constants for Renesas RZ/G2L family IRQC bindings.
  *
- * Copyright (C) 2022 Renesas Electronics Corp.
+ * Copyright (C) 2024 Renesas Electronics Corp.
  *
  */
 
@@ -12,7 +12,11 @@
 /* NMI maps to SPI0 */
 #define RZG2L_NMI	0
 
-/* IRQ0-7 map to SPI1-8 */
+/*
+ * - RZ/G2L Series: IRQ0-7 map to SPI1-8
+ * - RZ/[V2H, G3E]: IRQ0-15 map to SPI1-16
+ */
+
 #define RZG2L_IRQ0	1
 #define RZG2L_IRQ1	2
 #define RZG2L_IRQ2	3
@@ -21,5 +25,13 @@
 #define RZG2L_IRQ5	6
 #define RZG2L_IRQ6	7
 #define RZG2L_IRQ7	8
+#define RZG2L_IRQ8	9
+#define RZG2L_IRQ9	10
+#define RZG2L_IRQ10	11
+#define RZG2L_IRQ11	12
+#define RZG2L_IRQ12	13
+#define RZG2L_IRQ13	14
+#define RZG2L_IRQ14	15
+#define RZG2L_IRQ15	16
 
 #endif /* __DT_BINDINGS_IRQC_RZG2L_H */
