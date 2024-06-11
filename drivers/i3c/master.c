fe04835f149df44e06a1d98cd9c6b42ea24d0e9a
Au Dang <au.dang.jx@renesas.com>
2024-06-11 17:02:52 +0700

    Add i3c device userspace interface
    Currently there is no way to use i3c devices from user space,
    this patch will support userspace interface for I3C.
    
    Refer source:
    https://patchwork.kernel.org/project/linux-i3c/cover/cover.1575977795.git.vitor.soares@synopsys.com/
    
    Signed-off-by: Vitor Soares <Vitor.Soares@synopsys.com>
    
    Signed-off-by: Au Dang <au.dang.jx@renesas.com>

--- drivers/i3c/master.c
+++ drivers/i3c/master.c
@@ -339,6 +339,7 @@ struct bus_type i3c_bus_type = {
 	.probe = i3c_device_probe,
 	.remove = i3c_device_remove,
 };
+EXPORT_SYMBOL_GPL(i3c_bus_type);
 
 static enum i3c_addr_slot_status
 i3c_bus_get_addr_slot_status(struct i3c_bus *bus, u16 addr)
@@ -542,9 +543,10 @@ static void i3c_masterdev_release(struct device *dev)
 	of_node_put(dev->of_node);
 }
 
-static const struct device_type i3c_masterdev_type = {
+const struct device_type i3c_masterdev_type = {
 	.groups	= i3c_masterdev_groups,
 };
+EXPORT_SYMBOL_GPL(i3c_masterdev_type);
 
 static int i3c_bus_set_mode(struct i3c_bus *i3cbus, enum i3c_bus_mode mode,
 			    unsigned long max_i2c_scl_rate)
@@ -2813,6 +2815,18 @@ void i3c_dev_free_ibi_locked(struct i3c_dev_desc *dev)
 	kfree(dev->ibi);
 	dev->ibi = NULL;
 }
+
+int i3c_for_each_dev(void *data, int (*fn)(struct device *, void *))
+{
+	int res;
+
+	mutex_lock(&i3c_core_lock);
+	res = bus_for_each_dev(&i3c_bus_type, NULL, data, fn);
+	mutex_unlock(&i3c_core_lock);
+
+	return res;
+}
+EXPORT_SYMBOL_GPL(i3c_for_each_dev);
 
 static int __init i3c_init(void)
 {
