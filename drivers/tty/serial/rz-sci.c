d2c02fc3fb35c353e7b1202d2b48dff3237d07df
Nghia Vo <nghia.vo.zn@renesas.com>
2024-06-25 13:57:49 +0700

    serial: rz-sci: Fix infinite framing error warnings.
    RXDESEL: Select start receiving condition.
    - If this bit is 0, receiving process will start when RX line at LOW,
    this can lead to infinite framing error warnings.
    - If this bit is 1, the receiving process will start
    when FALLING EDGE detected. This option is recommended.
    
    Signed-off-by: Linh Hua <linh.hua.jc@renesas.com>
    Signed-off-by: Nghia Vo <nghia.vo.zn@renesas.com>

--- drivers/tty/serial/rz-sci.c
+++ drivers/tty/serial/rz-sci.c
@@ -1026,6 +1026,8 @@ done:
 
 	if (port->type == PORT_SCIF)
 		ccr3_val |= CCR3_FM;
+	if (port->type == PORT_SCI)
+		ccr3_val |= CCR3_RXDESEL;
 	serial_port_out(port, CCR3, ccr3_val);
 
 	ccr2_val |= (cks << 20) | (brr << 8);
