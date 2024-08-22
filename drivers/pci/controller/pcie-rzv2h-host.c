83ccd1b4d0fc9a0bfac923dac01b0f6e5cda30b8
Khai Nguyen <khai.nguyen.wx@renesas.com>
2024-08-22 14:07:01 +0700

    pci: controller: pcie-rzv2h: Add support PCIe for RZ/V2N
    This commit to add support PCIe Host for RZ/V2N SoC.
    
    Signed-off-by: Khai Nguyen <khai.nguyen.wx@renesas.com>

--- drivers/pci/controller/pcie-rzv2h-host.c
+++ drivers/pci/controller/pcie-rzv2h-host.c
@@ -1169,6 +1169,7 @@ static int rzv2h_pcie_parse_map_dma_ranges(struct rzv2h_pcie_host *host)
 static const struct of_device_id rzv2h_pcie_of_match[] = {
 	{ .compatible = "renesas,rzv2h-pcie", },
 	{ .compatible = "renesas,rzg3e-pcie", },
+	{ .compatible = "renesas,rzv2n-pcie", },
 	{},
 };
 
