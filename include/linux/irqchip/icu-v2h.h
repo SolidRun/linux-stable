cc2490bbb15e1de81ce25a6c11599c9fd4ad4ddd
Nghia Nguyen <nghia.nguyen.xm@renesas.com>
2024-11-05 14:45:46 +0700

    irqchip: irq-renesas-rzv2h: Fix DMAC ACK signal for RZ/V2H
    DMAC ACK signal value should be channel number.
    This patch fixes DMAC ACK signal for RZ/V2H.
    
    Signed-off-by: LongLuu <long.luu.ur@renesas.com>
    Signed-off-by: Nghia Nguyen <nghia.nguyen.xm@renesas.com>

--- include/linux/irqchip/icu-v2h.h
+++ include/linux/irqchip/icu-v2h.h
@@ -5,6 +5,6 @@
 
 int register_dmac_req_signal(struct platform_device *icu_dev,
 			unsigned int dmac, unsigned int channel, int dmac_req);
-int register_dmac_ack_signal(struct platform_device *icu_dev, int dmac_ack, int dmac_ack_value);
+int register_dmac_ack_signal(struct platform_device *icu_dev, int dmac_ack, int dmac_ack_channel);
 
 #endif
