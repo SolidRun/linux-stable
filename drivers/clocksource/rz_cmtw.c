a4f865a1d63889913d8b5a0d6480201cc47877d9
Hien Dang <hien.dang.xf@renesas.com>
2024-11-06 10:42:10 +0700

    clocksource: rz_cmtw: Add support for RZ/V2N
    Add compatible support for RZ/V2N in the rz_cmtw driver.
    
    Signed-off-by: Hien Dang <hien.dang.xf@renesas.com>

--- drivers/clocksource/rz_cmtw.c
+++ drivers/clocksource/rz_cmtw.c
@@ -392,6 +392,7 @@ static const struct rz_cmtw_info rzv2h_cmtw_info = {
 
 static const struct of_device_id rz_cmtw_of_table[] __maybe_unused = {
 	{ .compatible = "renesas,rzv2h-cmtw", .data = &rzv2h_cmtw_info },
+	{ .compatible = "renesas,rzv2n-cmtw", .data = &rzv2h_cmtw_info },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, rz_cmtw_of_table);
