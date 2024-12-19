31887ecafdc6ee0d3d0154ecd5bcdd4c161523b5
Minh Nguyen <minh.nguyen.kx@renesas.com>
2024-12-19 13:41:56 +0700

    can: rcar_canfd: Add support RZ/V2N CANFD
    Add CANFD compatible string support for RZ/V2N.
    RZ/V2N's CANFD is similar with RZ/V2H.
    
    Signed-off-by: Minh Nguyen <minh.nguyen.kx@renesas.com>

--- drivers/net/can/rcar/rcar_canfd.c
+++ drivers/net/can/rcar/rcar_canfd.c
@@ -2397,6 +2397,7 @@ static const __maybe_unused struct of_device_id rcar_canfd_of_table[] = {
 	{ .compatible = "renesas,rzg2l-canfd", .data = &rzg2l_hw_info },
 	{ .compatible = "renesas,r8a779a0-canfd", .data = &r8a779a0_hw_info },
 	{ .compatible = "renesas,r9a09g057-canfd", .data = &r9a09g057_hw_info },
+	{ .compatible = "renesas,r9a09g056-canfd", .data = &r9a09g057_hw_info },
 	{ }
 };
 
