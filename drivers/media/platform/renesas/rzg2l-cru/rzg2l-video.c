b8e41af40c483ed8cfaacbf3dfda860327c89ec1
Nghia Vo <nghia.vo.zn@renesas.com>
2024-12-19 14:40:39 +0700

    media: rzg2l-cru: Inline calculating bytesperline
    Remove the `rzg2l_cru_format_bytesperline()` function and inline the
    calculation of `bytesperline` directly in `rzg2l_cru_format_align()`.
    This simplifies the code by removing an unnecessary function call and
    directly multiplying the image width by the `bpp` (bytes per pixel)
    from the format structure.
    
    Signed-off-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Link: https://lore.kernel.org/r/20241018133446.223516-15-prabhakar.mahadev-lad.rj@bp.renesas.com
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Nghia Vo <nghia.vo.zn@renesas.com>

--- drivers/media/platform/renesas/rzg2l-cru/rzg2l-video.c
+++ drivers/media/platform/renesas/rzg2l-cru/rzg2l-video.c
@@ -1035,22 +1035,18 @@ error:
  * V4L2 stuff
  */
 
-static u32 rzg2l_cru_format_bytesperline(struct v4l2_pix_format *pix)
-{
-	const struct rzg2l_cru_ip_format *fmt;
-
-	fmt = rzg2l_cru_ip_format_to_fmt(pix->pixelformat);
-
-	return pix->width * fmt->bpp;
-}
-
 static void rzg2l_cru_format_align(struct rzg2l_cru_dev *cru,
 				   struct v4l2_pix_format *pix)
 {
+	const struct rzg2l_cru_ip_format *fmt;
 	const struct rzg2l_cru_info *info = cru->info;
 
-	if (!rzg2l_cru_ip_format_to_fmt(pix->pixelformat))
+	fmt = rzg2l_cru_ip_format_to_fmt(pix->pixelformat);
+
+	if (!fmt) {
 		pix->pixelformat = RZG2L_CRU_DEFAULT_FORMAT;
+		fmt = rzg2l_cru_ip_format_to_fmt(pix->pixelformat);
+	}
 
 	switch (pix->field) {
 	case V4L2_FIELD_TOP:
@@ -1069,7 +1065,7 @@ static void rzg2l_cru_format_align(struct rzg2l_cru_dev *cru,
 	v4l_bound_align_image(&pix->width, 320, info->max_width, 1,
 			      &pix->height, 240, info->max_height, 2, 0);
 
-	pix->bytesperline = rzg2l_cru_format_bytesperline(pix);
+	pix->bytesperline = pix->width * fmt->bpp;
 	pix->sizeimage = pix->bytesperline * pix->height;
 
 	dev_dbg(cru->dev, "Format %ux%u bpl: %u size: %u\n",
