f48dd06632dc8b0c7cd4bcd0ec8c2b066eba7b06
Nghia Vo <nghia.vo.zn@renesas.com>
2025-01-07 11:06:03 +0700

    media: rzg2l-cru: Set auto recovery mode when AXI Bus is congested for RZ/G2L series
    Sometimes, AXI bus congestion caused the image data being corrupted.
    This patch is created to set auto recovery mode when AXI Bus is
    congested.
    It will stop AXI master and clear the FIFO.
    After recovery, transfer resumes to the start address of the Memory Bank
    to which data was being transferred to.
    
    Because the detection of this issue is recognized by looping frame with
    a same HW memory bank, we update minimum buffer to 2 buffers to detect
    this issue.
    
    Signed-off-by: hienhuynh <hien.huynh.px@renesas.com>
    Signed-off-by: Nghia Vo <nghia.vo.zn@renesas.com>

--- drivers/media/platform/renesas/rzg2l-cru/rzg2l-video.c
+++ drivers/media/platform/renesas/rzg2l-cru/rzg2l-video.c
@@ -686,6 +686,22 @@ static irqreturn_t rzg2l_cru_irq(int irq, void *data)
 		cru->state = RZG2L_CRU_DMA_RUNNING;
 	}
 
+	if (slot != prev_slot[cru->id]) {
+		/* Update value of previous memory bank slot */
+		prev_slot[cru->id] = slot;
+	} else {
+		/*
+		 * AXI-Bus congestion maybe occurred.
+		 * Set auto recovery mode to clear all FIFOs
+		 * and resume transmission.
+		 */
+		rzg2l_cru_write(cru, AMnFIFO, 0);
+
+		dev_dbg(cru->dev, "Dropping frame %u with CRU channel %d\n",
+			cru->sequence, cru->id);
+		goto done;
+	}
+
 	/* Capture frame */
 	if (cru->queue_buf[slot]) {
 		cru->queue_buf[slot]->field = cru->format.field;
