ae4158ad47cf45d140caf4a32d4d825e37e4849d
Nghia Vo <nghia.vo.zn@renesas.com>
2025-01-07 14:39:03 +0700

    media: rzg2l-cru: Move auto recovery mode when AXI Bus is congested
    Move the function stop AXI master and clear the FIFO to avoid
    "Invalid MB address" while capturing.
    
    Signed-off-by: Nghia Vo <nghia.vo.zn@renesas.com>

--- drivers/media/platform/renesas/rzg2l-cru/rzg2l-video.c
+++ drivers/media/platform/renesas/rzg2l-cru/rzg2l-video.c
@@ -781,6 +781,22 @@ static irqreturn_t rzv2h_cru_irq(int irq, void *data)
 		goto done;
 	}
 
+	if (slot != prev_slot[cru->id]) {
+		/* Update value of previous memory bank slot */
+		prev_slot[cru->id] = slot;
+	} else {
+		/*
+		 * AXI-Bus congestion maybe occurred.
+		 * Set auto recovery mode to clear all FIFOs
+		 * and resume transmission.
+		 */
+		rzg2l_cru_write(cru, AMnFIFO, 0);
+
+		dev_dbg(cru->dev, "Dropping frame %u with CRU channel %d\n",
+			cru->sequence, cru->id);
+		goto done;
+	}
+
 	amnmbxaddrl[cru->id][slot] = 0;
 	amnmbxaddrh[cru->id][slot] = 0;
 
@@ -798,21 +814,6 @@ static irqreturn_t rzv2h_cru_irq(int irq, void *data)
 		cru->state = RZG2L_CRU_DMA_RUNNING;
 	}
 
-	if (slot != prev_slot[cru->id]) {
-		/* Update value of previous memory bank slot */
-		prev_slot[cru->id] = slot;
-	} else {
-		/*
-		 * AXI-Bus congestion maybe occurred.
-		 * Set auto recovery mode to clear all FIFOs
-		 * and resume transmission.
-		 */
-		rzg2l_cru_write(cru, AMnFIFO, 0);
-
-		dev_dbg(cru->dev, "Dropping frame %u with CRU channel %d\n",
-			cru->sequence, cru->id);
-		goto done;
-	}
 
 	/* Capture frame */
 	if (cru->queue_buf[slot]) {
