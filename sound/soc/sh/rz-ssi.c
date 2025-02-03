335f845295b0102934774da53c8a3274254a9481
Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
2025-02-03 16:02:14 +0700

    ASoC: renesas: rz-ssi: Issue software reset in hw_params API
    commit fc2a31affb22394d1d74d3ecc86b5c68da33d52a upstream.
    
    The code initially issued software reset on SNDRV_PCM_TRIGGER_START
    action only before starting the first stream. This can be easily moved to
    hw_params() as the action is similar to setting the clocks. Moreover,
    according to the hardware manual (Table 35.7 Bits Initialized by Software
    Reset of the SSIFCR.SSIRST Bit) the software reset action acts also on the
    clock dividers bits. Due to this issue the software reset in hw_params()
    before configuring the clock dividers. This also simplifies the code in
    trigger API.
    
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
    Link: https://patch.msgid.link/20241210170953.2936724-17-claudiu.beznea.uj@bp.renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sh/rz-ssi.c
+++ sound/soc/sh/rz-ssi.c
@@ -414,6 +414,15 @@ static int rz_ssi_start(struct rz_ssi_priv *ssi, struct rz_ssi_stream *strm)
 	return 0;
 }
 
+static int rz_ssi_swreset(struct rz_ssi_priv *ssi)
+{
+	u32 tmp;
+
+	rz_ssi_reg_mask_setl(ssi, SSIFCR, SSIFCR_SSIRST, SSIFCR_SSIRST);
+	rz_ssi_reg_mask_setl(ssi, SSIFCR, SSIFCR_SSIRST, 0);
+	return readl_poll_timeout_atomic(ssi->base + SSIFCR, tmp, !(tmp & SSIFCR_SSIRST), 1, 5);
+}
+
 static int rz_ssi_stop(struct rz_ssi_priv *ssi, struct rz_ssi_stream *strm)
 {
 	strm->running = 0;
@@ -833,14 +842,6 @@ static int rz_ssi_dai_trigger(struct snd_pcm_substream *substream, int cmd,
 
 	switch (cmd) {
 	case SNDRV_PCM_TRIGGER_START:
-		/* Soft Reset */
-		if (!rz_ssi_is_stream_running(&ssi->playback) &&
-		    !rz_ssi_is_stream_running(&ssi->capture)) {
-			rz_ssi_reg_mask_setl(ssi, SSIFCR, 0, SSIFCR_SSIRST);
-			rz_ssi_reg_mask_setl(ssi, SSIFCR, SSIFCR_SSIRST, 0);
-			udelay(5);
-		}
-
 		rz_ssi_stream_init(strm, substream);
 
 		if (rz_ssi_is_dma_enabled(ssi)) {
@@ -978,6 +979,7 @@ static int rz_ssi_dai_hw_params(struct snd_pcm_substream *substream,
 					SNDRV_PCM_HW_PARAM_SAMPLE_BITS)->min;
 	unsigned int channels = params_channels(params);
 	unsigned int rate = params_rate(params);
+	int ret;
 
 	ssi->sample_width = params_width(params);
 	if ((sample_bits != 16) && (ssi->sample_width == 16)) {
@@ -1004,6 +1006,10 @@ static int rz_ssi_dai_hw_params(struct snd_pcm_substream *substream,
 
 	rz_ssi_cache_hw_params(ssi, rate, channels, strm->sample_width,
 			       sample_bits);
+
+	ret = rz_ssi_swreset(ssi);
+	if (ret)
+		return ret;
 
 	return rz_ssi_clk_setup(ssi, rate, channels);
 }
