745fc790e345f26b19cba3654813fee309677ff3
Hiep Pham <hiep.pham.zy@renesas.com>
2025-03-13 11:39:50 +0700

    drivers: usb: host: xhci: Update USB3/PHY initial setting
    Update USB3/PHY initial setting with configuration of PIPE Status and
    Control Register. This is applied for RZ/V2{H,N} and RZ/G3E platforms.
    
    Signed-off-by: Hien Dang <hien.dang.xf@renesas.com>
    Signed-off-by: Hiep Pham <hiep.pham.zy@renesas.com>

--- drivers/usb/host/xhci-rzv2h.c
+++ drivers/usb/host/xhci-rzv2h.c
@@ -24,6 +24,13 @@
 #define USB3_HOST_INTEN_ENA			(USB3_HOST_INTEN_XHC \
 						| USB3_HOST_INTEN_HSE)
 
+/* PIPE Status and Control Register */
+#define USB3_HOST_U3P0PIPESC0			0x10C0
+#define USB3_HOST_U3P0PIPESC1			0x10C4
+#define USB3_HOST_U3P0PIPESC2			0x10C8
+#define USB3_HOST_U3P0PIPESC3			0x10CC
+#define USB3_HOST_U3P0PIPESC4			0x10D0
+
 /* USB3TEST Registers */
 #define USB3TEST_CREGCTRL			0x1010
 #define USB3TEST_CREGCTRL_ENTRY			BIT(0)
@@ -152,6 +159,13 @@ void xhci_rzv2h_start(struct usb_hcd *hcd)
 	u32 int_en;
 
 	if (hcd->regs) {
+		/* Update the controller initial setting */
+		writel(0x03130200, hcd->regs + USB3_HOST_U3P0PIPESC0);
+		writel(0x00160200, hcd->regs + USB3_HOST_U3P0PIPESC1);
+		writel(0x03150000, hcd->regs + USB3_HOST_U3P0PIPESC2);
+		writel(0x03130200, hcd->regs + USB3_HOST_U3P0PIPESC3);
+		writel(0x00180000, hcd->regs + USB3_HOST_U3P0PIPESC4);
+
 		/* Interrupt Enable */
 		int_en = readl(hcd->regs + USB3_HOST_INTEN);
 		int_en |= USB3_HOST_INTEN_ENA;
