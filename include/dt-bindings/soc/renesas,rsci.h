e72cde94855bb227335ac39d886e79cb4c9a59b7
Tranh Ha <tranh.ha.xb@renesas.com>
2025-04-16 11:27:03 +0700

    dt-bindings: soc: renesas,rsci: Add support for RSCI module
    Add macro UART, I2C, SPI function support for RSCI module.
    
    Signed-off-by: Tranh Ha <tranh.ha.xb@renesas.com>

---
+++ include/dt-bindings/soc/renesas,rsci.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+ *
+ * Copyright (C) 2025 Renesas Electronics Corp.
+ */
+
+#ifndef __DT_BINDINGS_RZV2H_RSCI_H
+#define __DT_BINDINGS_RZV2H_RSCI_H
+
+#define RSCI_UART	0
+#define RSCI_I2C	1
+#define RSCI_SPI	2
+
+#endif /* __DT_BINDINGS_RZV2H_RSCI_H */
