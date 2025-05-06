daa74a1e47cc80963defd02f45d787d885efe693
Au Dang <au.dang.jx@renesas.com>
2025-05-06 11:18:46 +0700

    mmc: host: renesas_sdhi_core: Fix delayed eMMC boot
    Add delay when tuning to avoid tuning issue on some SD cards when
    plug-in/out continuously.
    But this accidently apply for eMMC too and cause delayed boot sometimes.
    So avoid that (adding delay) in eMMC case.
    
    Refer work from "sang.nguyen-hoang@banvien.com.vn"
    
    Fix: "mmc: renesas_sdhi: add delay between tuning cmds"
    Signed-off-by: Au Dang <au.dang.jx@renesas.com>

--- drivers/mmc/host/renesas_sdhi_core.c
+++ drivers/mmc/host/renesas_sdhi_core.c
@@ -730,7 +730,8 @@ static int renesas_sdhi_execute_tuning(struct mmc_host *mmc, u32 opcode)
 			mmc_send_abort_tuning(mmc, opcode);
 
 		/* FIXME: Needed for some SD cards. The reason is not known yet */
-		usleep_range(1000, 2500);
+		if (!(host->mmc->caps & MMC_CAP_NONREMOVABLE))
+			usleep_range(1000, 1250);
 	}
 
 	ret = renesas_sdhi_select_tuning(host);
