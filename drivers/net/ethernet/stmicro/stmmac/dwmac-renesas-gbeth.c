cec1ed9984f0428b71e63c6af0d1cdccf455a010
Au Dang <au.dang.jx@renesas.com>
2025-07-18 16:23:48 +0700

    net: stmmac: dwmac-renesas-gbeth: Support set PHY interface for ETH0/1
    Set PHY interface using syscon based on phy-mode property in devicetree.
    
    Signed-off-by: Au Dang <au.dang.jx@renesas.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-renesas-gbeth.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-renesas-gbeth.c
@@ -18,13 +18,31 @@
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/reset.h>
+#include <linux/of.h>
+#include <linux/of_net.h>
+#include <linux/mfd/syscon.h>
+#include <linux/regmap.h>
 
 #include "stmmac_platform.h"
+
+#define SYS_GBETH_CFG(m)	(0xF00 + ((m) * 0x4))
+#define SYS_GBETH_CFG_PHY_MAC_SPEED_MASK	GENMASK(1, 0)
+#define SYS_GBETH_CFG_PHY_INTF_TYPE_MASK	GENMASK(18, 16)
+#define SYS_GBETH_CFG_PHY_INTF_TYPE_MII		0
+#define SYS_GBETH_CFG_PHY_INTF_TYPE_RGMII	1
+
+struct gbeth_hw_info {
+	 u8 max_gbeth_channels;
+};
 
 struct renesas_gbeth {
 	struct plat_stmmacenet_data *plat_dat;
 	struct reset_control *rstc;
 	struct device *dev;
+	struct regmap *syscon;
+	const struct gbeth_hw_info *info;
+	phy_interface_t phy_mode;
+	int id;
 };
 
 static const char *const renesas_gbeth_clks[] = {
@@ -36,8 +54,31 @@ static int renesas_gbeth_init(struct platform_device *pdev, void *priv)
 	struct plat_stmmacenet_data *plat_dat;
 	struct renesas_gbeth *gbeth = priv;
 	int ret;
+	u32 val, reg;
 
 	plat_dat = gbeth->plat_dat;
+
+	switch (gbeth->phy_mode) {
+		case PHY_INTERFACE_MODE_MII:
+			val = SYS_GBETH_CFG_PHY_INTF_TYPE_MII;
+			break;
+		case PHY_INTERFACE_MODE_RGMII:
+		case PHY_INTERFACE_MODE_RGMII_ID:
+		case PHY_INTERFACE_MODE_RGMII_RXID:
+		case PHY_INTERFACE_MODE_RGMII_TXID:
+			val = SYS_GBETH_CFG_PHY_INTF_TYPE_RGMII;
+			break;
+	default:
+		dev_err(&pdev->dev, "Unsupported phy mode %s\n",
+			phy_modes(gbeth->phy_mode));
+		return -EINVAL;
+	}
+
+	regmap_read(gbeth->syscon, SYS_GBETH_CFG(gbeth->id), &reg);
+	reg = FIELD_GET(SYS_GBETH_CFG_PHY_MAC_SPEED_MASK, reg);
+	val = FIELD_PREP(SYS_GBETH_CFG_PHY_INTF_TYPE_MASK, val) |
+	      FIELD_PREP(SYS_GBETH_CFG_PHY_MAC_SPEED_MASK, reg);
+	regmap_write(gbeth->syscon, SYS_GBETH_CFG(gbeth->id), val);
 
 	ret = reset_control_deassert(gbeth->rstc);
 	if (ret) {
@@ -74,8 +115,13 @@ static int renesas_gbeth_probe(struct platform_device *pdev)
 	struct stmmac_resources stmmac_res;
 	struct device *dev = &pdev->dev;
 	struct renesas_gbeth *gbeth;
+	const struct gbeth_hw_info *info;
 	unsigned int i;
 	int err;
+
+	info = of_device_get_match_data(dev);
+	if (!info)
+		return -ENODEV;
 
 	err = stmmac_get_platform_resources(pdev, &stmmac_res);
 	if (err)
@@ -90,6 +136,18 @@ static int renesas_gbeth_probe(struct platform_device *pdev)
 	gbeth = devm_kzalloc(dev, sizeof(*gbeth), GFP_KERNEL);
 	if (!gbeth)
 		return -ENOMEM;
+
+	gbeth->syscon = syscon_regmap_lookup_by_phandle_args(dev->of_node,
+					"renesas,eth-syscon", 1, &gbeth->id);
+	if (IS_ERR(gbeth->syscon))
+		return dev_err_probe(dev, PTR_ERR(gbeth->syscon),
+				"Failed to get ethernet syscon");
+
+	if (gbeth->id >= info->max_gbeth_channels) {
+		dev_err(dev, "Invalid gbeth id: %d (max %d)\n",
+			gbeth->id, info->max_gbeth_channels - 1);
+		return -EINVAL;
+	}
 
 	plat_dat->num_clks = ARRAY_SIZE(renesas_gbeth_clks);
 	plat_dat->clks = devm_kcalloc(dev, plat_dat->num_clks,
@@ -113,7 +171,12 @@ static int renesas_gbeth_probe(struct platform_device *pdev)
 	if (IS_ERR(gbeth->rstc))
 		return PTR_ERR(gbeth->rstc);
 
+	err = of_get_phy_mode(dev->of_node, &gbeth->phy_mode);
+	if (err)
+		return dev_err_probe(dev, err, "Failed to get phy mode\n");
+
 	gbeth->dev = dev;
+	gbeth->info = info;
 	gbeth->plat_dat = plat_dat;
 	plat_dat->bsp_priv = gbeth;
 	plat_dat->set_clk_tx_rate = stmmac_set_clk_tx_rate;
@@ -133,8 +196,12 @@ static int renesas_gbeth_probe(struct platform_device *pdev)
 	return 0;
 }
 
+static const struct gbeth_hw_info rzv2h_gbeth_info = {
+	.max_gbeth_channels = 2,
+};
+
 static const struct of_device_id renesas_gbeth_match[] = {
-	{ .compatible = "renesas,rzv2h-gbeth", },
+	{ .compatible = "renesas,rzv2h-gbeth", .data = &rzv2h_gbeth_info, },
 	{ /* Sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, renesas_gbeth_match);
