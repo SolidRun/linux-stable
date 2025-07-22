0c536712b6867cb9eff393f15b64db387b696b99
Nghia Vo <nghia.vo.zn@renesas.com>
2025-07-22 11:38:03 +0700

    media: rzg2l-cru: reinitialize the prev_slot value
    Reinitialize the prev_slot value (prev_slot = -1)
    after set AMnFIFO to 0 to avoid the case the next
    slot has similar number with prev_slot.
    
    Signed-off-by: Nghia Vo <nghia.vo.zn@renesas.com>

--- drivers/media/platform/renesas/rzg2l-cru/rzg2l-video.c
+++ drivers/media/platform/renesas/rzg2l-cru/rzg2l-video.c
@@ -748,6 +748,7 @@ irqreturn_t rzg2l_cru_irq(int irq, void *data)
 		 * and resume transmission.
 		 */
 		rzg2l_cru_write(cru, AMnFIFO, 0);
+		prev_slot[cru->id] = -1;
 
 		dev_dbg(cru->dev, "Dropping frame %u with CRU channel %d\n",
 			cru->sequence, cru->id);
@@ -870,6 +871,7 @@ irqreturn_t rzg3e_cru_irq(int irq, void *data)
 			 * and resume transmission.
 			 */
 			rzg2l_cru_write(cru, AMnFIFO, 0);
+			prev_slot[cru->id] = -1;
 
 			dev_dbg(cru->dev, "Dropping frame %u with CRU channel %d\n",
 				cru->sequence, cru->id);
