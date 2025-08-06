247e43a90fa07b936e099a6ec32d6c77e2331a1d
trong.le-van <trong.le-van@banvien.com.vn>
2025-08-06 15:16:21 +0700

    memory: renesas-rpc-if: Add xSPI compatible for G3S
    Adds support for the xSPI interface on the RZ/G3S (r9a08g045) SoC.
    It introduces a new `rpcif_info` entry for G3S, updates logic to
    handle XSPI_RZ_G3S in initialization, and adds the appropriate
    compatible string for device tree matching.
    
    Signed-off-by: trong.le-van <trong.le-van@banvien.com.vn>

--- drivers/memory/renesas-rpc-if.c
+++ drivers/memory/renesas-rpc-if.c
@@ -1106,6 +1106,7 @@ static const struct rpcif_info xspi_info_r9a09g047 = {
 };
 
 static const struct of_device_id rpcif_of_match[] = {
+	{ .compatible = "renesas,r9a08g045-xspi", .data = &xspi_info_r9a09g047 },
 	{ .compatible = "renesas,r9a09g047-xspi", .data = &xspi_info_r9a09g047 },
 	{ .compatible = "renesas,rcar-gen3-rpc-if", .data = &rpcif_info_gen3 },
 	{ .compatible = "renesas,rzg2l-rpc-if", .data = &rpcif_info_rz_g2l },
