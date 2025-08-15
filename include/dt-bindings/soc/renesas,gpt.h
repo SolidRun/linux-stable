a2ffb35427adb7993a62c0a6a5986ba9de4a2034
Thang Dinh <thang.dinh.fv@renesas.com>
2025-08-15 11:43:47 +0700

    dt-bindings: soc: renesas,gpt: Add support for GPT module
    Add macro CHANNEL_A, CHANNEL_B, BOTH_AB support for GPT module.
    
    Signed-off-by: Thang Dinh <thang.dinh.fv@renesas.com>

---
+++ include/dt-bindings/soc/renesas,gpt.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+ *
+ * Copyright (C) 2025 Renesas Electronics Corp.
+ */
+
+#ifndef __DT_BINDINGS_GPT_H
+#define __DT_BINDINGS_GPT_H
+
+#define GPT_CHANNEL_A	0
+#define GPT_CHANNEL_B	1
+#define BOTH_CHANNEL	2
+
+#endif /* __DT_BINDINGS_GPT_H */
+
