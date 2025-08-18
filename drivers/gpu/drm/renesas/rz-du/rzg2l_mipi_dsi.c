066d84c111837b83671c1d0290f5d496c88c6ff5
Nghia Vo <nghia.vo.zn@renesas.com>
2025-08-18 11:06:15 +0700

    drm: renesas: rz-du: rzg2l_mipi_dsi: add MIPI DSI support for RZ/G3E
    Add MIPI DSI support for RZ/G3E.
    RZ/G3E's MIPI DSI is similar with RZ/V2H.
    The only difference is RZ/G3E's MIPI DSI can select from one of
    two PLLDSI0/1 input clocks.
    
    Signed-off-by: hienhuynh <hien.huynh.px@renesas.com>
    Signed-off-by: Nghia Vo <nghia.vo.zn@renesas.com>

--- drivers/gpu/drm/renesas/rz-du/rzg2l_mipi_dsi.c
+++ drivers/gpu/drm/renesas/rz-du/rzg2l_mipi_dsi.c
@@ -1413,6 +1413,7 @@ static const struct rzg2l_mipi_dsi_hw_info rzg2l_mipi_dsi_info = {
 };
 
 static const struct of_device_id rzg2l_mipi_dsi_of_table[] = {
+	{ .compatible = "renesas,r9a09g047-mipi-dsi", .data = &rzv2h_mipi_dsi_info, },
 	{ .compatible = "renesas,r9a09g057-mipi-dsi", .data = &rzv2h_mipi_dsi_info, },
 	{ .compatible = "renesas,rzg2l-mipi-dsi", .data = &rzg2l_mipi_dsi_info, },
 	{ /* sentinel */ }
