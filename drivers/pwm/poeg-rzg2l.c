21012b52bef4e51f6f46a37f58ce60d3919d06e2
Sang Nguyen <sang.nguyen-hoang@banvien.com.vn>
2025-08-21 09:52:40 +0700

    pwm: poeg-rzg2l: Add compatible for RZ/G3S
    This patch adds RZ/G2L POEG driver compatible for RZ/G3S.
    
    Signed-off-by: Sang Nguyen <sang.nguyen-hoang@banvien.com.vn>

--- drivers/pwm/poeg-rzg2l.c
+++ drivers/pwm/poeg-rzg2l.c
@@ -189,6 +189,7 @@ static int rzg2l_poeg_remove(struct platform_device *pdev)
 
 static const struct of_device_id rzg2l_poeg_of_table[] = {
 	{ .compatible = "renesas,poeg-r9a07g044", },
+	{ .compatible = "renesas,poeg-r9a08g045", },
 	{ .compatible = "renesas,poeg-r9a09g057", },
 	{ },
 };
